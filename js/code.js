5f499cd5b56b36dd329bc2cfa266c8b08a311346
luntu sogeyana <sogeyanaluntu@gmail.com>
2024-01-11 08:49:02 +0200

    overhall

--- js/code.js
+++ js/code.js
@@ -1,93 +1,34 @@
-// //addition
-// function addition() {
-//     console.log(2 + 2);
-// }
-// // subtraction
-// function subtraction() {
-//     console.log(5 - 2);
-// }
-// // Multiplication
-// function multiplication() {
-//     console.log(2 * 2);
-// }
+let currentVal = '';
 
-// function division() {
-//     console.log(2 / 2);
-// }
-// function exponents() {
-//     console.log(2 ** 2);
-// }
-// function modulus(){
-//     console.log(5 % 2);
-// }
-// //call all functions
-// // addition();
-// // subtraction();
-// // multiplication();
-// // division();
-// // exponents();
-// // modulus()
-// // call
-// let numb1 = 3;
-// numb1 %= 2;
-// console.log(numb1);
+function appendValue(value) {
+    currentVal += value;
+    document.getElementById('result').value = currentVal;
+}
 
-function display(value) {
-    document.getElementById("result").value += value;
+function operate(operator) {
+    // This is to check if the last character to be entered is an operator
+    const lastChar = currentVal.slice(-1);
+    const operators = ['+', '-', '*', '/'];
+
+    // If the last character is not an operator, the user can add one
+    if (!operators.includes(lastChar)) {
+        currentVal += ` ${operator} `;
+        document.getElementById('result').value = currentVal;
+    }
 }
-let allowed = true;
- 
-// function calculate() {
-//     var p = document.getElementById("result").value;
-//     var q = eval(p);
-//     document.getElementById("result").value = q;
-// }
-function clear(params) {
-    
-}
+
 function clearScreen() {
-    document.getElementById("result").value = "";
+    currentVal = '';
+    document.getElementById('result').value = '';
 }
-function del(){
-    document.getElementById("result").value = document.getElementById("result").value.slice(0, -1)
-}
-function calculate(){
-    try{
-        document.getElementById("result").value = eval(document.getElementById("result").value);
-    }
-    catch(err)
-    {
-        alert("invalid");
-    }
-}
-function decimalCal (value) {
-    if (allowed === true) {
-        value += value ;
-        allowed = false;
-    }
-}
-function operator (value) {
-    value += value ;
-    allowed: true;
-}
-const toggleAllowed = () => {
-    allowed = true;
-}
-function _________() {
-    let x = (InputValue.value.length - 1);
-    if ((InputValue.value[x]).includes('.') ) {
-        return false;
-    }
-    else {
-        return true;
-    }
-}
-// decimal.addEventListener('click', function(){
-//     if (allowed) {
-//         result.value += decimal.textContent;
-//         allowed = false;
-//     }
-// })
 
-
-
+function calculate() {
+    try {
+        let result = eval(currentVal);
+        document.getElementById('result').value = result;
+        currentVal = result.toString();
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+        currentVal = '';
+    }
+}
